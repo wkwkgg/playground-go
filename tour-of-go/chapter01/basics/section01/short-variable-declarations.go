53b4975e7b14b96fd2f49b25b6108e25cef506b6
wkwkgg <yujin@komachi.live>
2021-04-19 20:27:11 +0900

    done basic/section01

---
+++ tour-of-go/chapter01/basics/section01/short-variable-declarations.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i, j int = 1, 2
+	// same as `var k int = 3`
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
+}
